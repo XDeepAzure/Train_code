294506af48208dca8f2643ba368795b54e99d92c
XDeepAzure <2211269917@qq.com>
2023-07-26 20:38:24 +0800

    ok

--- train.sh
+++ train.sh
@@ -1,9 +1,5 @@
 
-<<<<<<< Updated upstream
-CUDA_VISIBLE_DEVICES="1"  python3 ./train.py \
-=======
 CUDA_VISIBLE_DEVICES="2"  python3 ./train.py \
->>>>>>> Stashed changes
     --src_lang nl_XX \
     --tgt_lang zh_CN \
     --bi false  \
@@ -21,30 +17,17 @@ CUDA_VISIBLE_DEVICES="2"  python3 ./train.py \
     --denoising_langs "nl_XX,zh_CN" \
     --denoising_file "train.nl,train.zh"  \
     --train_strategy epoch \
-    --eval_strategy epoch \
-    --eval_step 1 \
-    --save_step 1 \
-    --log_step  400 \
-<<<<<<< Updated upstream
-    --num_epoch 15 \
-    --warmup_steps 0 \
-    --steps "translate" \
+    --eval_strategy steps \
+    --eval_step 2000 \
+    --save_step 2000 \
+    --log_step  200 \
+    --num_epoch 30 \
+    --warmup_steps 2000 \
+    --steps "translate,denoising" \
     --w_noise 0.5 \
-    --beta 0 \
-    --optimer adamw \
-    --metrics 'bleu,'\
-    --data_dir /data/hyxu/codes/lowMT_compute/data/public_data/train/pair \
-    --name 'ft-clean'     \
-# nohup ./train.sh > ./log/ft_clean.log 2>&1 &
-=======
-    --num_epoch 20 \
-    --warmup_steps 0 \
-    --steps "translate" \
-    --w_noise 1 \
     --optimer adamw \
     --metrics 'bleu,'\
     --data_dir /data/hyxu/lowMT_compute/data/public_data/train/pair \
-    --name 'ft'     \
-# nohup ./train.sh > ./log/ft.log 2>&1 &
->>>>>>> Stashed changes
+    --name 'ft-denoise1'     \
+# nohup ./train.sh > ./log/train_noise1.log 2>&1 &
 # bsub -n 1 -q HPC.S1.GPU.X795.suda -o ./log/train_mbart50.log -gpu  num=1:mode=exclusive_process sh ./src/nmt_train.sh
